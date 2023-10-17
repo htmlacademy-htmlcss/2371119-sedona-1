d0a5c7098aec822be83913bde886d4074754fa3a
Keks <keks@htmlacademy.ru>
2023-10-17 12:11:16 +0300

    :heavy_check_mark: Сборка #3

--- 3/index.js
+++ 3/index.js
@@ -1,12 +1,15 @@
 let infoButton = document.querySelector('.information-button');
 let kidsInfoBlock = document.querySelector('.kids-info');
-let modalButton = document.querySelector('.modal-button');
+let modalButtons = document.querySelectorAll('.modal-button');
 let modal = document.querySelector('.modal-container');
 let modalCloseButton = document.querySelector('.modal-close-button');
 let adults = document.querySelector('.adults-input');
 let kids = document.querySelector('.kids-input');
 let minusButtons = document.querySelectorAll('.minus-button');
 let plusButtons = document.querySelectorAll('.plus-button');
+const rangeInput = document.querySelectorAll('.price-range'),
+    progress = document.querySelector('.progress');
+
 
 infoButton.onmouseover = function () {
     kidsInfoBlock.classList.remove('visually-hidden');
@@ -16,9 +19,11 @@ infoButton.onmouseout = function () {
     kidsInfoBlock.classList.add('visually-hidden');
 }
 
-modalButton.onclick = function () {
-    modal.classList.remove('modal-closed');
-}
+modalButtons.forEach(modalButton => {
+    modalButton.onclick = function () {
+        modal.classList.remove('modal-closed');
+    };
+});
 
 modalCloseButton.onclick = function () {
     modal.classList.add('modal-closed');
@@ -27,9 +32,9 @@ modalCloseButton.onclick = function () {
 for (let minusButton of minusButtons) {
     minusButton.onclick = function () {
         if (minusButton.parentElement.classList.contains('adults-wrapper')) {
-            adults.value -= 1;
+            if (adults.value > 0) { adults.value -= 1; };
         } else {
-            kids.value -= 1;
+            if (kids.value > 0) { kids.value -= 1; }
         }
     }
 }
@@ -43,3 +48,24 @@ for (let plusButton of plusButtons) {
         }
     }
 }
+
+rangeInput.forEach(input => {
+    input.addEventListener("input", () => {
+        let minVal = parseInt(rangeInput[0].value),
+            maxVal = parseInt(rangeInput[1].value);
+
+        if (minVal > maxVal) {
+            let tmp = maxVal;
+            maxVal = minVal;
+            minVal = tmp;
+        }
+
+        progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+        progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+
+        let minDisplay = document.querySelector('.price-input-min'),
+            maxDisplay = document.querySelector('.price-input-max');
+        minDisplay.value = minVal;
+        maxDisplay.value = maxVal;
+    });
+});
